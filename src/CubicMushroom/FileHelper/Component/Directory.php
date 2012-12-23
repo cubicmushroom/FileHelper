ffd0d5a213e3a3a26440a1424ce443268dd751a6
Toby Griffiths <toby@cubicmushroom.co.uk>
2012-12-23 16:46:35 +0000

    Initial composer files & Directory::rrdir()

---
+++ src/CubicMushroom/FileHelper/Component/Directory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CubicMushroom\FileHelper;
+
+class Directory
+{
+    public function rrmdir($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+}
---
+++ src/CubicMushroom/FileHelper/FileHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CubicMushroom\FileHelper;
+
+use CubicMushroom\FileHelper\Component\Directory;
+use CubicMushroom\FileHelper\Component\File;
+
+class FileHelper
+{
+    $directory;
+    $file;
+
+    public function __construct()
+    {
+        $this->directory = new Directory();
+        $this->file      = new File();
+    }
+
+    /**
+     * @see CubicMushroom\FileHelper\Component\Directory::rrdir()
+     */
+    public function rrdir($dir)
+    {
+        $this->directory->rdir($dir);
+    }
+}
